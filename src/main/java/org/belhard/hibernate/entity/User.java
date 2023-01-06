6030dd86833d4cf1ef7d676521ce9864399a61f0
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-06 18:33:12 +0300

    Created User class and added basic methods

---
+++ src/main/java/org/belhard/hibernate/entity/User.java
@@ -0,0 +1,65 @@
+package org.belhard.hibernate.entity;
+
+import java.util.Objects;
+
+public class User {
+    private Long id;
+    private String login;
+    private String password;
+    private PersonalInfo personalInfo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public PersonalInfo getPersonalInfo() {
+        return personalInfo;
+    }
+
+    public void setPersonalInfo(PersonalInfo personalInfo) {
+        this.personalInfo = personalInfo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(login, user.login) && Objects.equals(password, user.password) && Objects.equals(personalInfo, user.personalInfo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password, personalInfo);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", personalInfo=" + personalInfo +
+                '}';
+    }
+}
